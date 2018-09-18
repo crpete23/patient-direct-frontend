6c5c4eb04d1e1fa08fef5b23537da6f25e5105dc
Christopher Peterson <crpete23@gmail.com>
2018-09-18 16:51:58 -0700

    started history population by clicking patient encounter, need to destructure the hpi and ros to complete, consider building an array of the information

--- src/components/physicians/SelectedEncounter.js
+++ src/components/physicians/SelectedEncounter.js
@@ -1,37 +1,24 @@
 import React from 'react'
-import {List} from 'semantic-ui-react'
+import {Grid, List} from 'semantic-ui-react'
 import { connect } from 'react-redux'
+import History from './History'
 
 
-const SelectedEncounter = ({id, date, dob, first_name, last_name, sex, time }) => {
+const SelectedEncounter = ({id, date, dob, first_name, last_name, sex, time, hx }) => {
   const dateOfBirth = `${dob.slice(5)}/${dob.slice(0,4)}`
 
   return (
-    <List as='ol'>
-  <List.Item as='li' value='*'>
-    {`${first_name} ${last_name}`}
-  </List.Item>
-  <List.Item as='li' value='*'>
-    User Benefits
-  </List.Item>
-  <List.Item as='li' value='*'>
-    User Types
-    <List.Item as='ol'>
-      <List.Item as='li' value='-'>
-        Admin
-      </List.Item>
-      <List.Item as='li' value='-'>
-        Power User
-      </List.Item>
-      <List.Item as='li' value='-'>
-        Regular User
-      </List.Item>
-    </List.Item>
-  </List.Item>
-  <List.Item as='li' value='*'>
-    Deleting Your Account
-  </List.Item>
-</List>
+    <Grid>
+      <Grid.Row>
+        <h1>{`${first_name} ${last_name}`}</h1>
+      </Grid.Row>
+      <Grid.Row>
+        <h3>{`Appointment Time: ${time}`}</h3>
+      </Grid.Row>
+      <Grid.Row>
+        <History {...hx} />
+      </Grid.Row>
+    </Grid>
 )
 }
 
