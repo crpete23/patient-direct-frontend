fa085c98510831b6546af4aa53f68a37d68b3862
Christopher Peterson <crpete23@gmail.com>
2018-09-25 11:16:40 -0700

    HPI is now being populated from HPI constant declared above component

--- src/components/checkIn/Hpi.js
+++ src/components/checkIn/Hpi.js
@@ -8,6 +8,58 @@ import RadioChoices from './RadioChoices'
 import CheckboxChoices from './CheckboxChoices'
 import WriteInAnswer from './WriteInAnswer'
 
+const hpiInfo = {
+  quality: {
+    type: 'radio',
+    choices: ['sharp', 'dull', 'pressure', 'ache'],
+    label: `What quality best describes your chest pain?`,
+    other: true
+  },
+  timing: {
+    type: 'radio',
+    choices: ['constant', 'intermittent', 'waxing and waning'],
+    label: `What is the frequency/ timing of your chest pain?`,
+    other: true
+  },
+  severity: {
+    type: 'radio',
+    choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    label: `What is the severity of your chest pain?`,
+    other: false
+  },
+  duration: {
+    type: 'radio',
+    choices: ['less than a minute', 'several minutes', 'one hour', 'several hours', 'one day', 'several days', 'several weeks', 'several months'],
+    label: 'When symptoms are present, how long do they usually last?',
+    other: true
+  },
+  location: {
+    type: 'radio',
+    choices: ['central chest', 'left chest', 'right chest', 'left shoulder/arm', 'right shoulder/arm'],
+    label: `What is the location of your chest pain?`,
+    other: true
+  },
+  alleviatingFactors: {
+    type: 'check',
+    choices: ['nitroglycerin', 'stretching or massaging the area', 'exertion', 'pain medications'],
+    label: 'What, if anything, makes your symptoms better? (select all that apply)'
+  },
+  exacerbatingFactors: {
+    type: 'check',
+    choices: ['movement', 'exertion', 'deep breathing', 'eating/drinking'],
+    label: 'What, if anything, induces or makes your symptoms worse? (select all that apply)'
+  },
+  associatedSx: {
+    type: 'check',
+    choices: ['shortness of breath', 'diaphoresis (sweating)', 'nausea', 'vomitting', 'fatigue', 'dizziness/light headedness'],
+    label: `Have you experienced any other symptoms that you feel are related to your chest pain? (select all that apply)`
+  },
+  context: {
+    type: 'write',
+    label: `Briefly describe the context of your chest pain`
+  }
+}
+
 class Hpi extends Component {
   state = {
 
@@ -64,21 +116,32 @@ class Hpi extends Component {
   render() {
     const cc = this.props.encounter.hx.hpi.cc[0]
 
+    const keyArr = Object.keys(hpiInfo)
+    let hpiJSX = keyArr.map(element => {
+      if (hpiInfo[element].type === 'radio'){
+        return (
+          <RadioChoices key={element} choices={hpiInfo[element].choices} element={element} cc={cc} passChange={this.passChange} label={hpiInfo[element].label} otherChoice={hpiInfo[element].other} />
+        )
+      }
+      if (hpiInfo[element].type === 'check'){
+        return (
+          <CheckboxChoices key={element} choices={hpiInfo[element].choices} element={element} cc={cc} passChange={this.passMultiple} label={hpiInfo[element].label} />
+        )
+      }
+      if (hpiInfo[element].type === 'write'){
+        return (
+          <WriteInAnswer key={element} element={element} cc={cc} passChange={this.passChange} label={hpiInfo[element].label} />
+        )
+      }
+    })
+
     return (
       <Grid columns={3}>
         <Grid.Column width={2}>
         </Grid.Column>
         <Grid.Column width={12}>
           <Form onSubmit={this.submitHPI}>
-            <RadioChoices choices={['sharp', 'dull', 'pressure', 'ache']} element={'quality'} cc={cc} passChange={this.passChange} label={`What quality best describes your ${cc}?`} otherChoice={true} />
-            <RadioChoices choices={['constant', 'intermittent', 'waxing and waning']} element={'timing'} cc={cc} passChange={this.passChange} label={`What is the frequency/ timing of your ${cc}?`} otherChoice={true} />
-            <RadioChoices choices={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']} element={'severity'} cc={cc} passChange={this.passChange} label={`What is the severity of your ${cc}?`} otherChoice={false} />
-            <RadioChoices choices={['less than a minute', 'several minutes', 'one hour', 'several hours', 'one day', 'several days', 'several weeks', 'several months']} element={'duration'} cc={cc} passChange={this.passChange} label={'When symptoms are present, how long do they usually last?'} otherChoice={true} />
-            <RadioChoices choices={['central chest', 'left chest', 'right chest', 'left shoulder/arm', 'right shoulder/arm']} element={'location'} cc={cc} passChange={this.passChange} label={`What is the location of your ${cc}?`} otherChoice={true} />
-            <CheckboxChoices choices={['nitroglycerin', 'stretching or massaging the area', 'exertion', 'pain medications']} element={'alleviatingFactors'} cc={cc} passChange={this.passMultiple} label={'What, if anything, makes your symptoms better? (select all that apply)'} />
-            <CheckboxChoices choices={['movement', 'exertion', 'deep breathing', 'eating/drinking']} element={'exacerbatingFactors'} cc={cc} passChange={this.passMultiple} label={'What, if anything, induces or makes your symptoms worse? (select all that apply)'} />
-            <CheckboxChoices choices={['shortness of breath', 'diaphoresis (sweating)', 'nausea', 'vomitting', 'fatigue', 'dizziness/light headedness']} element={'associatedSx'} cc={cc} passChange={this.passMultiple} label={`Have you experienced any other symptoms that you feel are related to your ${cc}? (select all that apply)`} />
-            <WriteInAnswer element={'context'} cc={cc} passChange={this.passChange} label={`Briefly describe the context of your ${cc}`} />
+            {hpiJSX}
             <Form.Button>Submit</Form.Button>
           </Form>
         </Grid.Column>
