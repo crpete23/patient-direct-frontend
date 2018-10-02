61cc03631f6bddea60a3116ab775b9aabd4620ab
Christopher Peterson <crpete23@gmail.com>
2018-10-02 13:47:52 -0700

    added checked in label if patient has checked in

--- src/components/physicians/Encounter.js
+++ src/components/physicians/Encounter.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {List} from 'semantic-ui-react'
+import {List, Grid, Label} from 'semantic-ui-react'
 import { getEncounterById } from '../../actions/encounters'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 
-const Encounter = ({id, date, dob, first_name, last_name, sex, time, getEncounterById }) => {
+const Encounter = ({id, date, dob, first_name, last_name, sex, time, getEncounterById, checkedIn }) => {
   const dateOfBirth = `${dob.slice(5)}/${dob.slice(0,4)}`
 
   function upperCase (string) {
@@ -16,7 +16,14 @@ const Encounter = ({id, date, dob, first_name, last_name, sex, time, getEncounte
     <List.Item onClick={()=>getEncounterById(id, date)}>
      <List.Content>
       <List.Header>{time}</List.Header>
-      {`Patient: ${upperCase(first_name)} ${upperCase(last_name)}, Sex: ${upperCase(sex)}, Date of Birth: ${dateOfBirth}`}
+      <Grid>
+        <Grid.Column width={2}>
+          {checkedIn? <Label color='green'>Checked In</Label>: <Label color='grey'>Not Arrived</Label> }
+        </Grid.Column>
+        <Grid.Column width={14}>
+          {`Patient: ${upperCase(first_name)} ${upperCase(last_name)}, Sex: ${upperCase(sex)}, Date of Birth: ${dateOfBirth}`}
+        </Grid.Column>
+      </Grid>
      </List.Content>
    </List.Item>
   )
